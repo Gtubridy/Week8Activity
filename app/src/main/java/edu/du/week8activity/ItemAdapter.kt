277463775f8684b2f64a92dd02f63420ee4d01a4
BryanRoss1 <bryanpross76@gmail.com>
2023-11-02 10:34:29 -0600

    Added two new layouts and changed the number of entries to 30

--- app/src/main/java/edu/du/week8activity/ItemAdapter.kt
+++ app/src/main/java/edu/du/week8activity/ItemAdapter.kt
@@ -16,6 +16,8 @@ class ItemAdapter(context: Context, private var items: ArrayList<ListItem>) :
     companion object {
         const val VIEW_TYPE_ONE = 1
         const val VIEW_TYPE_TWO = 2
+        const val VIEW_TYPE_THREE = 3
+        const val VIEW_TYPE_FOUR = 4
     }
 
     private val context: Context = context
@@ -48,6 +50,36 @@ class ItemAdapter(context: Context, private var items: ArrayList<ListItem>) :
 
     }
 
+    private inner class TypeThreeViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+        private var currentItem: ListItem? = null
+
+        fun bind(item: ListItem) {
+            currentItem = item
+
+            val uri: Uri = Uri.parse("https://robohash.org/" + item.name + "?set=set1")
+            itemView.findViewById<TextView>(R.id.item_title).text = item.name
+            loadImage(context, itemView.findViewById(R.id.item_image), uri)
+        }
+
+    }
+
+    private inner class TypeFourViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+        private var currentItem: ListItem? = null
+
+        fun bind(item: ListItem) {
+            currentItem = item
+
+            val uri: Uri = Uri.parse("https://robohash.org/" + item.name + "?set=set2")
+            val uri2: Uri = Uri.parse("https://robohash.org/" + item.name + "?set=set3")
+            itemView.findViewById<TextView>(R.id.item_title).text = item.name
+            loadImage(context, itemView.findViewById(R.id.item_image), uri)
+            loadImage(context, itemView.findViewById(R.id.item_image2), uri2)
+        }
+
+    }
+
     fun loadImage(context: Context, imageView: ImageView, uri: Uri) {
         Glide
             .with(context)
@@ -62,9 +94,15 @@ class ItemAdapter(context: Context, private var items: ArrayList<ListItem>) :
         if (viewType == VIEW_TYPE_ONE) {
             return TypeOneViewHolder(LayoutInflater.from(parent.context)
                 .inflate(R.layout.item_type_one, parent, false))
-        } else {
+        } else if (viewType == VIEW_TYPE_TWO) {
             return TypeTwoViewHolder(LayoutInflater.from(parent.context)
                 .inflate(R.layout.item_type_two, parent, false))
+        } else if (viewType == VIEW_TYPE_THREE) {
+            return TypeThreeViewHolder(LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_type_three, parent, false))
+        } else {
+            return TypeFourViewHolder(LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_type_four, parent, false))
         }
     }
 
@@ -79,8 +117,15 @@ class ItemAdapter(context: Context, private var items: ArrayList<ListItem>) :
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         if (items[position].viewType == VIEW_TYPE_ONE) {
             (holder as TypeOneViewHolder).bind(items[position])
-        } else {
+        }
+        else if (items[position].viewType == VIEW_TYPE_TWO) {
             (holder as TypeTwoViewHolder).bind(items[position])
+        }
+        else if (items[position].viewType == VIEW_TYPE_THREE) {
+            (holder as TypeThreeViewHolder).bind(items[position])
+        }
+        else {
+            (holder as TypeFourViewHolder).bind(items[position])
         }
     }
 
--- app/src/main/java/edu/du/week8activity/MainActivity.kt
+++ app/src/main/java/edu/du/week8activity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +13,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val list = ArrayList<ListItem>()
-        for (i in 0..22) {
-            val itemType = if (i % 2 == 1) ItemAdapter.VIEW_TYPE_ONE else ItemAdapter.VIEW_TYPE_TWO
+        for (i in 0..30) {
+            var itemType: Int
+            if (i<20) {
+                itemType = if (i % 2 == 1) ItemAdapter.VIEW_TYPE_ONE else ItemAdapter.VIEW_TYPE_TWO
+            } else {
+                itemType = if (i % 2 == 1) ItemAdapter.VIEW_TYPE_THREE else ItemAdapter.VIEW_TYPE_FOUR
+            }
             val name = if (i % 2 == 1) "" + Random.nextInt(0, 50) else Char('A'.code + i).toString()
             list.add(ListItem(itemType, name))
         }
